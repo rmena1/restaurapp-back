a59b3366e57a4698f41bbac2d1777ec4ae2c903a
rmena1 <rma@uc.cl>
2022-07-02 17:02:53 -0400

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 import os
-
+from waitress import serve
 from flask import Flask
 
 app = Flask(__name__, instance_relative_config=True)
@@ -10,3 +10,8 @@ app.config.from_mapping(
 
 from . import routes
 app.register_blueprint(routes.bp)
+
+if __name__ == "__main__":
+    app.debug = False
+    port = int(os.environ.get('PORT', 4000))
+    serve(app, port=port)

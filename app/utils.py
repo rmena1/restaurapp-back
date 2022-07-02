4bcb65b73576dc7698bbe7bcba6a5339241daf71
rmena1 <rma@uc.cl>
2022-07-02 16:45:03 -0400

    fix

--- app/utils.py
+++ app/utils.py
@@ -5,5 +5,5 @@ import json
 def get_data():
     print('get_data()')
     data = requests.get('https://storage.googleapis.com/backupdatadev/ejercicio/ventas.json')
-    print('data sample:', data[0])
+    print('data ready')
     return json.loads(data.content)

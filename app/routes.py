8206e7510520cfbd8dc85f030f582094052c9750
rmena1 <rma@uc.cl>
2022-07-04 10:23:57 -0400

    feat: useful info from data

--- app/routes.py
+++ app/routes.py
@@ -30,3 +30,16 @@ def get_by_exact_date(date):
     filtered_data = [d for d in data if d['date_closed'].split(' ')[0] == date]
     ordered_data = utils.order_by_date(filtered_data)
     return jsonify(ordered_data)
+
+# get useful info from data between date range
+@bp.route('/get_info/<date_from>/<date_to>')
+def get_info(date_from, date_to):
+    data = utils.get_data()
+    filtered_data = [d for d in data if d['date_closed'].split(' ')[0] >= date_from and d['date_closed'].split(' ')[0] <= date_to]
+    # get total sales
+    total_sales = sum([d['total'] for d in filtered_data])
+    # avarage total sales
+    avarage_sales = total_sales / len(filtered_data)
+    # avarege diners
+    avarage_diners = sum([d['diners'] for d in filtered_data]) / len(filtered_data)
+    return jsonify({'total_sales': total_sales, 'avarage_sales': avarage_sales, 'avarage_diners': avarage_diners})
